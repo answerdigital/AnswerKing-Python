c82ae669737f74442eaeb00d2cd143b4a85a0c5e
Pietro Convalle <pietro.convalle@answerdigital.com>
2022-10-18 13:38:47 +0100

    moved SECRET_KEY into environ (.env config file)

--- answerking/settings.py
+++ answerking/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x977=v5a2q-e%_5$b9ge@jzk%s_nhk2l8_use&h@*m$w33dopr"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
