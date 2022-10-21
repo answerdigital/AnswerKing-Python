13b1eb0c004278322a7def0f58c4acd5dc3a0b55
PietroConvalleAD <116072211+PietroConvalleAD@users.noreply.github.com>
2022-10-21 16:31:14 +0100

    Bench 47 refactor with typing test items (#10)
    * Adds typing to items tests
    * moved ErrorType in views
    
    Co-authored-by: pietro convalle <pietroconvalle@python.it>

---
+++ answerking_app/tests/API_types.py
@@ -0,0 +1,17 @@
+from typing import TypedDict
+
+
+class NewItemType(TypedDict):
+    name: str
+    price: str
+    description: str
+    stock: int
+    calories: int
+
+
+class ItemIDType(TypedDict):
+    id: int
+
+
+class ItemType(ItemIDType, NewItemType):
+    pass
