937cdb60c642b5827bf660e7453bb59ddb3655b5
why743 <115239141+NogaAD@users.noreply.github.com>
2022-10-25 11:39:18 +0100

    black formatting

--- answerking_app/urls/order_urls.py
+++ answerking_app/urls/order_urls.py
@@ -7,7 +7,9 @@ from answerking_app.views import order_views
 urlpatterns: list[partial] = [
     path("orders", order_views.OrderListView.as_view(), name="order_list"),
     path(
-        "orders/<order_id>", order_views.OrderDetailView.as_view(), name="order_detail"
+        "orders/<order_id>",
+        order_views.OrderDetailView.as_view(),
+        name="order_detail",
     ),
     path(
         "orders/<order_id>/orderline/<item_id>",
