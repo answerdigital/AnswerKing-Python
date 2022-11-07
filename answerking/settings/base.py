e2f5d496ce8718e5b74aeb05dc475805c14f948e
pietro convalle <pietroconvalle@python.it>
2022-11-07 12:19:52 +0000

    fixed missing import error during rebase

--- answerking/settings/base.py
+++ answerking/settings/base.py
@@ -4,6 +4,10 @@ Django base settings for answerking project.
 import os
 from pathlib import Path
 
+from corsheaders.defaults import default_headers, default_methods
+
+from answerking_app.utils.json404_middleware_config import json404_response
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
