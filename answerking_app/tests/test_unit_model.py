fcd4b6952becfd10b2ba7e0909667821fe424866
Joseph Sykes <joseph.c.sykes@outlook.com>
2022-11-25 15:44:45 +0000

    added more tests

--- answerking_app/tests/test_unit_model.py
+++ answerking_app/tests/test_unit_model.py
@@ -6,13 +6,20 @@ from answerking_app.models.models import Order, Product, LineItem
 from answerking_app.tests.BaseTestClass import TestBase
 
 
-class ModelTests(TestBase, TestCase):
+class ModelTests(TestCase):
+    TB = TestBase()
+
+    def setUp(self) -> None:
+        self.TB.setUp()
+
+    def tearDown(self) -> None:
+        self.TB.tearDown()
 
     def test_calculate_total_with_line_items(self):
         test_order: Order = Order.objects.create()
-        test_order_line_1 = LineItem.objects.create(order=test_order, product=self.test_product_1, quantity=2)
-        test_order_line_2 = LineItem.objects.create(order=test_order, product=self.test_product_2, quantity=1)
-        test_order_line_3 = LineItem.objects.create(order=test_order, product=self.test_product_3, quantity=2)
+        test_order_line_1 = LineItem.objects.create(order=test_order, product=self.TB.test_product_1, quantity=2)
+        test_order_line_2 = LineItem.objects.create(order=test_order, product=self.TB.test_product_2, quantity=1)
+        test_order_line_3 = LineItem.objects.create(order=test_order, product=self.TB.test_product_3, quantity=2)
         test_order_line_1.calculate_sub_total()
         test_order_line_2.calculate_sub_total()
         test_order_line_3.calculate_sub_total()
@@ -20,10 +27,10 @@ class ModelTests(TestBase, TestCase):
         test_order.calculate_total()
 
         calculated_tot: Decimal = test_order.order_total
-        expected_tot: Decimal = ((self.test_product_1.price * 2)
-                                 + self.test_product_2.price
-                                 + (self.test_product_3.price * 2)
-                                 )
+        expected_tot: Decimal = Decimal((self.TB.test_product_1.price * 2)
+                                        + self.TB.test_product_2.price
+                                        + (self.TB.test_product_3.price * 2)
+                                        ).quantize(Decimal('0.00'))
 
         self.assertEqual(calculated_tot, expected_tot)
 
@@ -38,21 +45,21 @@ class ModelTests(TestBase, TestCase):
 
     def test_calculate_total_without_sub_total_calculated(self):
         test_order: Order = Order.objects.create()
-        test_order_line_1 = LineItem.objects.create(order=test_order, product=self.test_product_1, quantity=2)
+        test_order_line_1 = LineItem.objects.create(order=test_order, product=self.TB.test_product_1, quantity=2)
         test_order.calculate_total()
 
         calculated_tot: Decimal = test_order.order_total
         expected_tot: Decimal = Decimal(0.00)
 
         self.assertEqual(calculated_tot, expected_tot)
-        self.assertEqual(test_order_line_1.product.id, self.test_product_1.id)
+        self.assertEqual(test_order_line_1.product.id, self.TB.test_product_1.id)
 
     def test_calculate_sub_total(self):
         test_order: Order = Order.objects.create()
-        test_order_line_1 = LineItem.objects.create(order=test_order, product=self.test_product_1, quantity=2)
+        test_order_line_1 = LineItem.objects.create(order=test_order, product=self.TB.test_product_1, quantity=2)
         test_order_line_1.calculate_sub_total()
 
         calculated_sub_tot: Decimal = test_order_line_1.sub_total
-        expected_sub_tot: Decimal = self.test_product_1.price * 2
+        expected_sub_tot: Decimal = self.TB.test_product_1.price * 2
 
         self.assertEqual(calculated_sub_tot, expected_sub_tot)
--- answerking_app/tests/test_unit_serializer.py
+++ answerking_app/tests/test_unit_serializer.py
@@ -16,9 +16,16 @@ from answerking_app.utils.mixins.ApiExceptions import ProblemDetails
 from answerking_app.utils.model_types import ProductType
 
 
-class SerializerTests(TestBase, TestCase):
+class SerializerTests(TestCase):
 
     serializer_path = 'answerking_app.models.serializers.'
+    TB = TestBase()
+
+    def setUp(self) -> None:
+        self.TB.setUp()
+
+    def tearDown(self) -> None:
+        self.TB.tearDown()
 
     def test_compress_white_spaces_fn(self):
         test_str: str = "  White   Spaces    "
@@ -28,34 +35,34 @@ class SerializerTests(TestBase, TestCase):
         self.assertEqual(expected, actual)
 
     def test_cat_det_serializer_contains_correct_fields(self):
-        data: ReturnDict = self.test_cat_det_serializer.data
+        data: ReturnDict = self.TB.test_cat_det_serializer.data
         self.assertCountEqual(data.keys(), ['id', 'name', 'description'])
 
     def test_cat_det_serializer_id_field_content(self):
-        data: ReturnDict = self.test_cat_det_serializer.data
-        self.assertEqual(data['id'], self.test_cat_1.id)
+        data: ReturnDict = self.TB.test_cat_det_serializer.data
+        self.assertEqual(data['id'], self.TB.test_cat_1.id)
 
     def test_cat_det_serializer_name_field_content(self):
-        data: ReturnDict = self.test_cat_det_serializer.data
-        self.assertEqual(data['name'], self.test_cat_1.name)
+        data: ReturnDict = self.TB.test_cat_det_serializer.data
+        self.assertEqual(data['name'], self.TB.test_cat_1.name)
 
     def test_cat_det_serializer_name_max_length_fail(self):
-        serializer_data = self.cat_det_serializer_data
+        serializer_data = self.TB.cat_det_serializer_data
         serializer_data['name'] = 'e'*51
         serializer: CategoryDetailSerializer = CategoryDetailSerializer(data=serializer_data)
         self.assertFalse(serializer.is_valid())
-        self.assertEqual(set(serializer.errors), set('name'))
+        self.assertEqual(set(serializer.errors), {'name'})
 
     def test_cat_det_serializer_name_blank_fail(self):
-        serializer_data = self.cat_det_serializer_data
+        serializer_data = self.TB.cat_det_serializer_data
         serializer_data['name'] = ''
         serializer: CategoryDetailSerializer = CategoryDetailSerializer(data=serializer_data)
         self.assertFalse(serializer.is_valid())
-        self.assertEqual(set(serializer.errors), set('name'))
+        self.assertEqual(set(serializer.errors), {'name'})
 
     def test_cat_det_serializer_name_regex_validator_pass_1(self):
-        allowed_characters: str = 'abcdefghijklmnopqrstuvwxyz '
-        serializer_data = self.cat_det_serializer_data
+        allowed_characters: str = 'abcdefghijklm nopqrstuvwxyz'
+        serializer_data = self.TB.cat_det_serializer_data
         serializer_data['name'] = allowed_characters
         serializer = CategoryDetailSerializer(data=serializer_data)
         self.assertTrue(serializer.is_valid())
@@ -63,7 +70,7 @@ class SerializerTests(TestBase, TestCase):
 
     def test_cat_det_serializer_name_regex_validator_pass_2(self):
         allowed_characters: str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ!'
-        serializer_data = self.cat_det_serializer_data
+        serializer_data = self.TB.cat_det_serializer_data
         serializer_data['name'] = allowed_characters
         serializer = CategoryDetailSerializer(data=serializer_data)
         self.assertTrue(serializer.is_valid())
@@ -71,32 +78,32 @@ class SerializerTests(TestBase, TestCase):
 
     def test_cat_det_serializer_name_regex_validator_fail(self):
         allowed_characters: str = 'abcdefghijklmnopqrstuvwxyz '
-        serializer_data = self.cat_det_serializer_data
+        serializer_data = self.TB.cat_det_serializer_data
         serializer_data['name'] = allowed_characters + '#'
         serializer = CategoryDetailSerializer(data=serializer_data)
         self.assertFalse(serializer.is_valid())
-        self.assertEqual(set(serializer.errors), set('name'))
+        self.assertEqual(set(serializer.errors), {'name'})
 
     def test_cat_det_serializer_desc_field_content(self):
-        data: ReturnDict = self.test_cat_det_serializer.data
-        self.assertEqual(data['description'], self.test_cat_1.description)
+        data: ReturnDict = self.TB.test_cat_det_serializer.data
+        self.assertEqual(data['description'], self.TB.test_cat_1.description)
 
     def test_cat_det_serializer_desc_max_length_fail(self):
-        serializer_data = self.cat_det_serializer_data
+        serializer_data = self.TB.cat_det_serializer_data
         serializer_data['description'] = 'e'*201
         serializer: CategoryDetailSerializer = CategoryDetailSerializer(data=serializer_data)
         self.assertFalse(serializer.is_valid())
-        self.assertEqual(set(serializer.errors), set('description'))
+        self.assertEqual(set(serializer.errors), {'description'})
 
     def test_cat_det_serializer_desc_None_pass(self):
-        serializer_data = self.cat_det_serializer_data
+        serializer_data = self.TB.cat_det_serializer_data
         serializer_data['description'] = None
         serializer: CategoryDetailSerializer = CategoryDetailSerializer(data=serializer_data)
         self.assertTrue(serializer.is_valid())
         self.assertEqual(serializer.data['description'], None)
 
     def test_cat_det_serializer_desc_blank_pass(self):
-        serializer_data = self.cat_det_serializer_data
+        serializer_data = self.TB.cat_det_serializer_data
         serializer_data['description'] = ''
         serializer: CategoryDetailSerializer = CategoryDetailSerializer(data=serializer_data)
         self.assertTrue(serializer.is_valid())
@@ -104,7 +111,7 @@ class SerializerTests(TestBase, TestCase):
 
     def test_cat_det_serializer_desc_regex_validator_pass_1(self):
         allowed_characters: str = 'abcdefghijklmnopqrstuvwxyz .!'
-        serializer_data = self.cat_det_serializer_data
+        serializer_data = self.TB.cat_det_serializer_data
         serializer_data['description'] = allowed_characters
         serializer = CategoryDetailSerializer(data=serializer_data)
         self.assertTrue(serializer.is_valid())
@@ -112,21 +119,19 @@ class SerializerTests(TestBase, TestCase):
 
     def test_cat_det_serializer_desc_regex_validator_pass_2(self):
         allowed_characters: str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ,#'
-        serializer_data = self.cat_det_serializer_data
+        serializer_data = self.TB.cat_det_serializer_data
         serializer_data['description'] = allowed_characters
         serializer = CategoryDetailSerializer(data=serializer_data)
         self.assertTrue(serializer.is_valid())
-        expected = allowed_characters+'~'
-        actual = serializer.data['description']
-        self.assertEqual(actual, expected)
+        self.assertEqual(serializer.data['description'], allowed_characters)
 
     def test_cat_det_serializer_desc_regex_validator_fail(self):
         allowed_characters: str = 'abcdefghijklmnopqrstuvwxyz '
-        serializer_data = self.cat_det_serializer_data
-        serializer_data['description'] = allowed_characters + '*'
+        serializer_data = self.TB.cat_det_serializer_data
+        serializer_data['description'] = allowed_characters+'*'
         serializer = CategoryDetailSerializer(data=serializer_data)
         self.assertFalse(serializer.is_valid())
-        self.assertEqual(set(serializer.errors), set('name'))
+        self.assertEqual(set(serializer.errors), {'description'})
 
     @mock.patch(
         serializer_path + 'CategoryDetailSerializer.products_check',
@@ -134,38 +139,40 @@ class SerializerTests(TestBase, TestCase):
     def test_cat_det_create_fn_no_products_pass(self, products_check_mock):
         products_check_mock.return_value = []
         cds = CategoryDetailSerializer()
-        new_cat = cds.create(self.cat_det_serializer_data)
+        new_cat = cds.create(self.TB.cat_det_serializer_data)
         created_cat = Category.objects.get(pk=new_cat.id)
         products_check_mock.assert_called_once()
-        cat_name = created_cat.name
-        self.assertEqual(cat_name, 'blah')  # self.cat_det_serializer_data['name']
-        self.assertEqual(created_cat.description, self.cat_det_serializer_data['description'])
-        self.assertEqual(created_cat.products, [])
+        self.assertEqual(created_cat.name, self.TB.cat_det_serializer_data['name'])
+        self.assertEqual(created_cat.description, self.TB.cat_det_serializer_data['description'])
+        self.assertEqual(list(created_cat.products.values()), [])
 
     @mock.patch(
         serializer_path + 'CategoryDetailSerializer.products_check',
     )
     def test_cat_det_create_fn_w_products_pass(self, products_check_mock):
-        products_check_mock.return_value = [self.test_product_1, self.test_product_2]
+        products_check_mock.return_value = [self.TB.test_product_1, self.TB.test_product_2]
         cds = CategoryDetailSerializer()
-        new_cat = cds.create(self.cat_det_serializer_data)
+        new_cat = cds.create(self.TB.cat_det_serializer_data)
         created_cat = Category.objects.get(pk=new_cat.id)
+        expected_prod_ids = [self.TB.test_product_1,
+                             self.TB.test_product_2
+                             ]
+        actual_prod_ids = list(created_cat.products.all())
         products_check_mock.assert_called_once()
-        self.assertEqual(created_cat.name, self.cat_det_serializer_data['name'])
-        self.assertEqual(created_cat.description, self.cat_det_serializer_data['description'])
-        self.assertEqual(created_cat.products, [self.test_product_1, self.test_product_2])
+        self.assertEqual(created_cat.name, self.TB.cat_det_serializer_data['name'])
+        self.assertEqual(created_cat.description, self.TB.cat_det_serializer_data['description'])
+        self.assertEqual(actual_prod_ids, expected_prod_ids)
 
-    # @mock.patch(
-    #     serializer_path + 'CategoryDetailSerializer.products_check',
-    # )
-    def test_cat_det_update_fn_retired_category_fail(self): # , products_check_mock):
-        self.assertEqual(2, 'bob')
-        # with self.assertRaises(ProblemDetails) as cm:
-        #     retired_cat = self.test_cat_retired
-        #     cds = CategoryDetailSerializer()
-        #     cds.update(retired_cat, self.cat_det_serializer_data)
-        #
-        # products_check_mock.assert_not_called()
-        # self.assertRaises(ProblemDetails)
-        # self.assertEqual(cm.exception.detail, "Bob")
-        # self.assertEqual(cm.exception.status_code, status.HTTP_410_GONE)
+    @mock.patch(
+        serializer_path + 'CategoryDetailSerializer.products_check',
+    )
+    def test_cat_det_update_fn_retired_category_fail(self, products_check_mock):
+        with self.assertRaises(ProblemDetails) as cm:
+            retired_cat = self.TB.test_cat_retired
+            cds = CategoryDetailSerializer()
+            cds.update(retired_cat, self.TB.cat_det_serializer_data)
+
+        products_check_mock.assert_not_called()
+        self.assertRaises(ProblemDetails)
+        self.assertEqual(cm.exception.detail, "This category has been retired")
+        self.assertEqual(cm.exception.status_code, status.HTTP_410_GONE)
