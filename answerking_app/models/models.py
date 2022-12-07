4bc7d1fd0c8e4519f1b44e95df56bfe9cbbbeea3
Joseph Sykes <joseph.c.sykes@outlook.com>
2022-12-07 16:45:31 +0000

    run make prepare

--- answerking_app/models/models.py
+++ answerking_app/models/models.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 
 from django.db import models
 
+
 class Product(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=200, blank=True, null=True)
