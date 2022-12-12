8ac75c3f621b0a7de268398fffa5fb5720bf5893
NogaAD <115239141+NogaAD@users.noreply.github.com>
2022-12-12 13:54:42 +0000

    more tests

--- answerking_app/tests/test_unit/test_unit_order_serializer.py
+++ answerking_app/tests/test_unit/test_unit_order_serializer.py
@@ -12,13 +12,21 @@ from answerking_app.tests.test_unit.UnitTestBaseClass import UnitTestBase
 class OrderSerializerUnitTests(UnitTestBase):
     UBT = UnitTestBase()
     serializer_path: str = "answerking_app.models.serializers."
-    test_prod_data: dict = UBT.get_fixture(
+    test_prod_1_data: dict = UBT.get_fixture(
         "products",
         "plain_burger_data.json"
     )
-    test_order_data: dict = UBT.get_fixture(
+    test_prod_2_data: dict = UBT.get_fixture(
+        "products",
+        "margarita_pizza_data.json"
+    )
+    test_order_1_data: dict = UBT.get_fixture(
         "orders",
         "order_data.json"
+    )
+    test_order_2_data: dict = UBT.get_fixture(
+        "orders",
+        "order_data_2.json"
     )
     frozen_time: str = "2022-01-01T01:02:03.000000Z"
     frozen_time_update: str = "2022-07-01T01:32:03.000000Z"
@@ -26,8 +34,11 @@ class OrderSerializerUnitTests(UnitTestBase):
     @freeze_time(frozen_time)
     def setUp(self):
         order: Order = Order.objects.create()
-        product: Product = Product.objects.create(**self.test_prod_data)
-        line_item = LineItem.objects.create(order=order, product=product, quantity=3)
+        product_burger: Product = Product.objects.create(**self.test_prod_1_data)
+        self.test_order_1_data['lineItems'][0]['product']['id'] = product_burger.id
+        product_pizza: Product = Product.objects.create(**self.test_prod_2_data)
+        self.test_order_2_data['lineItems'][0]['product']['id'] = product_pizza.id
+        line_item = LineItem.objects.create(order=order, product=product_burger, quantity=3)
         line_item.calculate_sub_total()
         order.calculate_total()
 
@@ -37,7 +48,7 @@ class OrderSerializerUnitTests(UnitTestBase):
         LineItem.objects.all().delete()
 
     def test_order_serializer_contains_correct_fields(self):
-        order: Order = Order.objects.all().get()
+        order: Order = Order.objects.first()
         test_serializer_data: ReturnDict = OrderSerializer(order).data
         expected: list[str] = [
             "id",
@@ -51,7 +62,7 @@ class OrderSerializerUnitTests(UnitTestBase):
         self.assertEqual(actual, expected)
 
     def test_order_serializer_id_field_content(self):
-        order: Order = Order.objects.all().get()
+        order: Order = Order.objects.first()
         test_serializer_data: ReturnDict = OrderSerializer(order).data
         expected: int = order.id
         actual: int = test_serializer_data['id']
@@ -90,7 +101,7 @@ class OrderSerializerUnitTests(UnitTestBase):
 
     @freeze_time(frozen_time)
     def test_order_serializer_created_on_and_last_updated_field_content(self):
-        order: Order = Order.objects.all().get()
+        order: Order = Order.objects.first()
         test_serializer_data: ReturnDict = OrderSerializer(order).data
         expected: str = self.frozen_time
         actual_created: str = test_serializer_data["createdOn"]
@@ -104,12 +115,12 @@ class OrderSerializerUnitTests(UnitTestBase):
         return_value=[],
     )
     def test_valid_order_create(self, products_check_mock):
-        new_order_data = OrderSerializer(data=self.test_order_data)
+        new_order_data = OrderSerializer(data=self.test_order_1_data)
         new_order_data.is_valid()
         serializer = OrderSerializer()
         new_order_object = serializer.create(new_order_data.validated_data)
         new_order_serializer_data: ReturnDict = OrderSerializer(new_order_object).data
-        test_json_data = self.test_order_data["lineItems"][0]
+        test_json_data = self.test_order_1_data["lineItems"][0]
 
         expected_product_id: dict = new_order_object.lineitem_set.values('product_id')[0]
         actual_product_id: dict = {'product_id': test_json_data['product']['id']}
@@ -164,8 +175,8 @@ class OrderSerializerUnitTests(UnitTestBase):
 
         expected_total: Decimal = Decimal(0.00)
         actual_total: Decimal = new_order_object.order_total
-        products_check_mock.assert_not_called()
 
+        products_check_mock.assert_not_called()
         self.assertEqual(actual_total, expected_total)
         self.assertEqual(actual_total, expected_total)
         self.assertEqual(actual_created, expected_time)
@@ -181,12 +192,12 @@ class OrderSerializerUnitTests(UnitTestBase):
     )
     def test_valid_order_update(self, products_check_mock):
         old_order: Order = Order.objects.all().get()
-        updated_order_data = OrderSerializer(data=self.test_order_data)
+        updated_order_data = OrderSerializer(data=self.test_order_2_data)
         updated_order_data.is_valid()
         serializer = OrderSerializer()
         new_order_object = serializer.update(old_order, updated_order_data.validated_data)
         new_order_serializer_data: ReturnDict = OrderSerializer(new_order_object).data
-        test_json_data = self.test_order_data["lineItems"][0]
+        test_json_data = self.test_order_2_data["lineItems"][0]
 
         expected_product_id: dict = new_order_object.lineitem_set.values('product_id')[0]
         actual_product_id: dict = {'product_id': test_json_data['product']['id']}
@@ -196,20 +207,51 @@ class OrderSerializerUnitTests(UnitTestBase):
 
         expected_create_time: str = self.frozen_time
         actual_created: str = new_order_serializer_data["createdOn"]
+
         expected_update_time: str = self.frozen_time_update
         actual_updated: str = new_order_serializer_data["lastUpdated"]
 
         expected_status: str = "Created"
         actual_status: str = new_order_object.order_status
 
-        expected_total: Decimal = Decimal(60.00)
+        expected_total: Decimal = Decimal(42.00)
         actual_total: Decimal = new_order_object.order_total
-        products_check_mock.assert_called_once()
 
-        self.assertEqual(actual_total, expected_total)
+        products_check_mock.assert_called_once()
+        self.assertEqual(new_order_object.lineitem_set.count(), 1)
         self.assertEqual(actual_product_id, expected_product_id)
         self.assertEqual(actual_total, expected_total)
         self.assertEqual(actual_quantity, expected_quantity)
+        self.assertEqual(actual_created, expected_create_time)
+        self.assertEqual(actual_updated, expected_update_time)
+        self.assertEqual(actual_status, expected_status)
+
+    @freeze_time(frozen_time_update)
+    @mock.patch(
+        serializer_path + "OrderSerializer.products_check",
+        return_value=[],
+    )
+    def test_valid_order_update_with_empty_order(self, products_check_mock):
+        old_order: Order = Order.objects.first()
+        serializer = OrderSerializer()
+        new_order_object = serializer.update(old_order, {})
+        new_order_serializer_data: ReturnDict = OrderSerializer(new_order_object).data
+
+        expected_create_time: str = self.frozen_time
+        actual_created: str = new_order_serializer_data["createdOn"]
+
+        expected_update_time: str = self.frozen_time_update
+        actual_updated: str = new_order_serializer_data["lastUpdated"]
+
+        expected_status: str = "Created"
+        actual_status: str = new_order_object.order_status
+
+        expected_total: Decimal = Decimal(0.00)
+        actual_total: Decimal = new_order_object.order_total
+
+        products_check_mock.assert_not_called()
+        self.assertEqual(new_order_object.lineitem_set.count(), 0)
+        self.assertEqual(actual_total, expected_total)
         self.assertEqual(actual_created, expected_create_time)
         self.assertEqual(actual_updated, expected_update_time)
         self.assertEqual(actual_status, expected_status)
