2fe21286bff959f46e8626185254afc0a0127d76
Joseph Sykes <joseph.c.sykes@outlook.com>
2022-12-15 15:06:02 +0000

    created integration tests for products without categories

--- answerking_app/tests/test_integration/snapshots/snap_test_products.py
+++ answerking_app/tests/test_integration/snapshots/snap_test_products.py
@@ -21,11 +21,11 @@ snapshots["DeleteTests::test_delete_invalid_id_returns_bad_request 1"] = {
     "type": "http://testserver/problems/error/",
 }
 
-snapshots["DeleteTests::test_delete_non_existent_id_returns_not_found 1"] = {
-    "detail": "Not Found",
-    "status": 404,
-    "title": "Resource not found",
-    "type": "http://testserver/problems/error/",
+snapshots['DeleteTests::test_delete_non_existent_id_returns_not_found 1'] = {
+    'detail': 'Not Found',
+    'status': 404,
+    'title': 'Resource not found',
+    'type': 'http://testserver/problems/error/'
 }
 
 snapshots[
@@ -133,58 +133,50 @@ snapshots["GetTests::test_get_invalid_id_returns_bad_request 1"] = {
     "type": "http://testserver/problems/error/",
 }
 
-snapshots["GetTests::test_get_non_existent_id_returns_not_found 1"] = {
-    "detail": "Not Found",
-    "status": 404,
-    "title": "Resource not found",
-    "type": "http://testserver/problems/error/",
+snapshots['GetTests::test_get_invalid_id_returns_bad_request 1'] = {
+    'detail': 'Invalid parameters',
+    'status': 400,
+    'title': 'Request has invalid parameters',
+    'type': 'http://testserver/problems/error/'
 }
 
-snapshots["PostTests::test_post_duplicated_name_returns_400 1"] = {
-    "detail": "This name already exists",
-    "status": 400,
-    "title": "A server error occurred.",
-    "type": "http://testserver/problems/error/",
+snapshots['GetTests::test_get_non_existent_id_returns_not_found 1'] = {
+    'detail': 'Not Found',
+    'status': 404,
+    'title': 'Resource not found',
+    'type': 'http://testserver/problems/error/'
 }
 
-snapshots[
-    "PostTests::test_post_invalid_data_returns_bad_request_1_invalid_id_json 1"
-] = {
-    "detail": "Validation Error",
-    "errors": {"id": ["A valid integer is required."]},
-    "status": 400,
-    "title": "Invalid input.",
-    "type": "http://testserver/problems/error/",
+snapshots['PostTests::test_post_duplicated_name_returns_400 1'] = {
+    'detail': 'This name already exists',
+    'status': 400,
+    'title': 'A server error occurred.',
+    'type': 'http://testserver/problems/error/'
 }
 
-snapshots[
-    "PostTests::test_post_invalid_data_returns_bad_request_2_invalid_name_json 1"
-] = {
-    "detail": "Validation Error",
-    "errors": {
-        "name": [
-            "Enter a valid value.",
-            "Ensure this field has no more than 50 characters.",
+snapshots['PostTests::test_post_invalid_data_returns_bad_request_1_invalid_id_json 1'] = {
+    'detail': 'Validation Error',
+    'errors': {
+        'id': [
+            'A valid integer is required.'
         ]
     },
-    "status": 400,
-    "title": "Invalid input.",
-    "type": "http://testserver/problems/error/",
+    'status': 400,
+    'title': 'Invalid input.',
+    'type': 'http://testserver/problems/error/'
 }
 
-snapshots[
-    "PostTests::test_post_invalid_data_returns_bad_request_3_invalid_description_json 1"
-] = {
-    "detail": "Validation Error",
-    "errors": {
-        "description": [
-            "Enter a valid value.",
-            "Ensure this field has no more than 200 characters.",
+snapshots['PostTests::test_post_invalid_data_returns_bad_request_2_invalid_name_json 1'] = {
+    'detail': 'Validation Error',
+    'errors': {
+        'name': [
+            'Enter a valid value.',
+            'Ensure this field has no more than 50 characters.'
         ]
     },
-    "status": 400,
-    "title": "Invalid input.",
-    "type": "http://testserver/problems/error/",
+    'status': 400,
+    'title': 'Invalid input.',
+    'type': 'http://testserver/problems/error/'
 }
 
 snapshots[
@@ -306,47 +298,36 @@ snapshots[
             "Ensure this field has no more than 50 characters.",
         ]
     },
-    "status": 400,
-    "title": "Invalid input.",
-    "type": "http://testserver/problems/error/",
+    'status': 400,
+    'title': 'Invalid input.',
+    'type': 'http://testserver/problems/error/'
 }
 
-snapshots[
-    "PutTests::test_put_invalid_data_returns_bad_request_3_invalid_description_json 1"
-] = {
-    "detail": "Validation Error",
-    "errors": {
-        "description": [
-            "Enter a valid value.",
-            "Ensure this field has no more than 200 characters.",
+snapshots['PostTests::test_post_invalid_data_returns_bad_request_4_invalid_retired_json 1'] = {
+    'detail': 'Validation Error',
+    'errors': {
+        'retired': [
+            'Must be a valid boolean.'
         ]
     },
-    "status": 400,
-    "title": "Invalid input.",
-    "type": "http://testserver/problems/error/",
+    'status': 400,
+    'title': 'Invalid input.',
+    'type': 'http://testserver/problems/error/'
 }
 
-snapshots[
-    "PutTests::test_put_invalid_data_returns_bad_request_4_invalid_retired_json 1"
-] = {
-    "detail": "Validation Error",
-    "errors": {"retired": ["Must be a valid boolean."]},
-    "status": 400,
-    "title": "Invalid input.",
-    "type": "http://testserver/problems/error/",
-}
-
-snapshots[
-    "PutTests::test_put_invalid_data_returns_bad_request_5_invalid_missing_fields_1_json 1"
-] = {
-    "detail": "Validation Error",
-    "errors": {
-        "name": ["This field is required."],
-        "price": ["This field is required."],
+snapshots['PostTests::test_post_invalid_data_returns_bad_request_5_invalid_missing_fields_1_json 1'] = {
+    'detail': 'Validation Error',
+    'errors': {
+        'name': [
+            'This field is required.'
+        ],
+        'price': [
+            'This field is required.'
+        ]
     },
-    "status": 400,
-    "title": "Invalid input.",
-    "type": "http://testserver/problems/error/",
+    'status': 400,
+    'title': 'Invalid input.',
+    'type': 'http://testserver/problems/error/'
 }
 
 snapshots[
@@ -362,26 +343,79 @@ snapshots[
     "type": "http://testserver/problems/error/",
 }
 
-snapshots["PutTests::test_put_invalid_id_returns_bad_request 1"] = {
-    "detail": "Invalid parameters",
-    "status": 400,
-    "title": "Request has invalid parameters",
-    "type": "http://testserver/problems/error/",
+snapshots['PostTests::test_post_invalid_json_returns_bad_request 1'] = {
+    'detail': 'Parsing JSON Error',
+    'errors': 'JSON parse error - Expecting value: line 1 column 13 (char 12)',
+    'status': 400,
+    'title': 'Invalid input json.',
+    'type': 'http://testserver/problems/error/'
 }
 
-snapshots["PutTests::test_put_invalid_json_returns_bad_request 1"] = {
-    "detail": "Parsing JSON Error",
-    "errors": "JSON parse error - Expecting value: line 1 column 13 (char 12)",
-    "status": 400,
-    "title": "Invalid input json.",
-    "type": "http://testserver/problems/error/",
+snapshots['PostTests::test_post_valid_returns_ok_1_basic_1_json 1'] = [
+    {
+        'categories': [
+        ],
+        'description': 'desc',
+        'id': 1,
+        'name': 'Burger',
+        'price': 1.2,
+        'retired': False
+    }
+]
+
+snapshots['PostTests::test_post_valid_returns_ok_2_boundry_name_json 1'] = [
+    {
+        'categories': [
+        ],
+        'description': 'desc',
+        'id': 1,
+        'name': 'fifty chars Lorem ipsum dolor sit amet consectetur',
+        'price': 1.2,
+        'retired': False
+    }
+]
+
+snapshots['PostTests::test_post_valid_returns_ok_3_boundry_description_json 1'] = [
+    {
+        'categories': [
+        ],
+        'description': 'twohundred chars Lorem ipsum dolor sit amet consectetur adipiscing elit Duis lacinia dui et nunc hendrerit ullamcorper Duis sit amet magna ac velit auctor porta Praesent viverra vulputate lectusjaue',
+        'id': 1,
+        'name': 'Burger',
+        'price': 1.2,
+        'retired': False
+    }
+]
+
+snapshots['PostTests::test_post_valid_returns_ok_4_boundry_price_json 1'] = [
+    {
+        'categories': [
+        ],
+        'description': 'desc',
+        'id': 1,
+        'name': 'Burger',
+        'price': 2147483647.0,
+        'retired': False
+    }
+]
+
+snapshots['PutTests::test_put_duplicated_name_returns_400 1'] = {
+    'detail': 'This name already exists',
+    'status': 400,
+    'title': 'A server error occurred.',
+    'type': 'http://testserver/problems/error/'
 }
 
-snapshots["PutTests::test_put_non_existent_id_returns_not_found 1"] = {
-    "detail": "Not Found",
-    "status": 404,
-    "title": "Resource not found",
-    "type": "http://testserver/problems/error/",
+snapshots['PutTests::test_put_invalid_data_returns_bad_request_1_invalid_id_json 1'] = {
+    'detail': 'Validation Error',
+    'errors': {
+        'id': [
+            'A valid integer is required.'
+        ]
+    },
+    'status': 400,
+    'title': 'Invalid input.',
+    'type': 'http://testserver/problems/error/'
 }
 
 snapshots["PutTests::test_put_valid_returns_ok_1_basic_1_update_json 1"] = [
