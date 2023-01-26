574b817a4c42f1df68e67321c4bc1ca394a011c0
Joseph Sykes <joseph.c.sykes@outlook.com>
2023-01-26 16:34:19 +0000

    finish snapshots for categories GET requests and start snapshots for categories POST requests

--- answerking_app/tests/test_integration/snapshots/snap_test_categories.py
+++ answerking_app/tests/test_integration/snapshots/snap_test_categories.py
@@ -453,3 +453,110 @@ snapshots[
         "retired": False,
     }
 ]
+
+snapshots['GetTests::test_get_id_valid_returns_ok 1'] = {
+    'createdOn': '2022-01-01T01:02:03.000000Z',
+    'description': 'Juicy Burgers',
+    'id': 1,
+    'lastUpdated': '2022-01-01T01:02:03.000000Z',
+    'name': 'Burgers',
+    'products': [
+    ],
+    'retired': False
+}
+
+snapshots['GetTests::test_get_id_valid_with_products_returns_ok 1'] = {
+    'createdOn': '2022-01-01T01:02:03.000000Z',
+    'description': 'Juicy Burgers',
+    'id': 1,
+    'lastUpdated': '2022-01-01T01:02:03.000000Z',
+    'name': 'Burgers',
+    'products': [
+        1
+    ],
+    'retired': False
+}
+
+snapshots['GetTests::test_get_invalid_id_returns_bad_request 1'] = {
+    'detail': 'Invalid parameters',
+    'status': 400,
+    'title': 'Request has invalid parameters',
+    'type': 'http://testserver/problems/error/'
+}
+
+snapshots['GetTests::test_get_non_existent_id_returns_not_found 1'] = {
+    'detail': 'Not Found',
+    'status': 404,
+    'title': 'Resource not found',
+    'type': 'http://testserver/problems/error/'
+}
+
+snapshots['GetTests::test_get_prods_in_cat_invalid_id_returns_bad_request 1'] = {
+    'detail': 'Invalid parameters',
+    'status': 400,
+    'title': 'Request has invalid parameters',
+    'type': 'http://testserver/problems/error/'
+}
+
+snapshots['GetTests::test_get_prods_in_cat_non_existent_id_returns_not_found 1'] = {
+    'detail': 'Not Found',
+    'status': 404,
+    'title': 'Resource not found',
+    'type': 'http://testserver/problems/error/'
+}
+
+snapshots['GetTests::test_get_prods_in_cat_returns_ok 1'] = [
+    {
+        'categories': [
+            {
+                'description': 'Juicy Burgers',
+                'id': 1,
+                'name': 'Burgers'
+            }
+        ],
+        'description': 'desc',
+        'id': 1,
+        'name': 'Burger',
+        'price': 1.2,
+        'retired': False
+    }
+]
+
+snapshots['PostTests::test_post_valid_returns_ok_1_basic_1_post_json 1'] = [
+    {
+        'createdOn': '2022-01-01T01:02:03.000000Z',
+        'description': 'Juicy Burgers',
+        'id': 4,
+        'lastUpdated': '2022-01-01T01:02:03.000000Z',
+        'name': 'Burgers',
+        'products': [
+        ],
+        'retired': False
+    }
+]
+
+snapshots['PostTests::test_post_valid_returns_ok_2_boundary_name_json 1'] = [
+    {
+        'createdOn': '2022-01-01T01:02:03.000000Z',
+        'description': 'Juicy Burgers',
+        'id': 5,
+        'lastUpdated': '2022-01-01T01:02:03.000000Z',
+        'name': 'fifty chars Lorem ipsum dolor sit amet consectetur',
+        'products': [
+        ],
+        'retired': False
+    }
+]
+
+snapshots['PostTests::test_post_valid_returns_ok_3_boundary_description_json 1'] = [
+    {
+        'createdOn': '2022-01-01T01:02:03.000000Z',
+        'description': 'two hundred chars Lorem ipsum dolor sit amet consectetur adipiscing elit Duis lacinia dui et nunc hendrerit ullamcorper Duis sit amet magna ac velit auctor porta Praesent viverra vulputate lectusjaue',
+        'id': 6,
+        'lastUpdated': '2022-01-01T01:02:03.000000Z',
+        'name': 'Burgers',
+        'products': [
+        ],
+        'retired': False
+    }
+]
