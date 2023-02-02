c04942ce61f6945d64d82e6dc55363f0b74cf299
michael-osullivan <michael.osullivan@answerdigital.com>
2023-02-02 09:24:33 +0000

    Update action and add back docker action

--- terraform/ecs_fargate/ecs.tf
+++ terraform/ecs_fargate/ecs.tf
@@ -9,7 +9,7 @@ resource "aws_ecs_cluster" "ecs_cluster" {
 
 resource "aws_ecs_service" "service" {
   depends_on = [
-                module.rds_serverless_cluster_setup.rds_cluster_instance_endpoint, 
+                module.rds_serverless_cluster_setup.rds_cluster_instance_endpoint,
                 aws_iam_role.ecs_task_execution_role
                 ]
   name            = "${var.project_name}-ecs-service"
@@ -21,7 +21,7 @@ resource "aws_ecs_service" "service" {
 
   network_configuration {
    security_groups  = [aws_security_group.ecs_sg.id]
-   subnets          = [module.vpc_subnet_setup.public_subnet_ids[0]]	
+   subnets          = [module.vpc_subnet_setup.public_subnet_ids[0]]
    assign_public_ip = true
  }
 
@@ -41,7 +41,7 @@ resource "aws_ecs_task_definition" "task_definition" {
   container_definitions = jsonencode([
     {
       name      = "${var.project_name}-container"
-      image     = "${var.image_url}"
+      image     = "${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${var.project_name}-repo:latest"
       cpu       = 256
       essential = true
       networkMode = var.network_mode
@@ -67,11 +67,11 @@ resource "aws_ecs_task_definition" "task_definition" {
         {"name": "SECRET_KEY", "value": var.django_secret_key},
         {"name": "DJANGO_SETTINGS_MODULE", "value": var.django_settings_module},
         {"name": "DATABASE_ENGINE", "value": var.django_database_engine}
-    
+
       ]
     }
   ])
-  
+
   tags = {
     Name  = "${var.project_name}-task-definition"
     Owner = var.owner
