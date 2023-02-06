f926ebb962890a7f1a6145a1ec3c50d6f2d23498
Joseph Sykes <joseph.c.sykes@outlook.com>
2023-02-06 14:46:33 +0000

    written PUT and DELETE integration tests for tags endpoints

--- answerking_app/tests/test_integration/snapshots/snap_test_tags.py
+++ answerking_app/tests/test_integration/snapshots/snap_test_tags.py
@@ -7,6 +7,27 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
+snapshots['DeleteTests::test_delete_already_retired_returns_gone 1'] = {
+    'detail': 'This object has already been retired',
+    'status': 410,
+    'title': 'A server error occurred.',
+    'type': 'http://testserver/problems/error/'
+}
+
+snapshots['DeleteTests::test_delete_invalid_id_returns_bad_request 1'] = {
+    'detail': 'Invalid parameters',
+    'status': 400,
+    'title': 'Request has invalid parameters',
+    'type': 'http://testserver/problems/error/'
+}
+
+snapshots['DeleteTests::test_delete_non_existent_id_returns_not_found 1'] = {
+    'detail': 'Not Found',
+    'status': 404,
+    'title': 'Resource not found',
+    'type': 'http://testserver/problems/error/'
+}
+
 snapshots['GetTests::test_get_all_with_tags_returns_ok_1_basic_3_json 1'] = [
     {
         'description': 'Tag identifying a product as halal.',
@@ -229,6 +250,109 @@ snapshots['PostTests::test_post_valid_with_products_returns_ok_1_basic_1_with_pr
     }
 ]
 
+snapshots['PutTests::test_put_duplicated_name_returns_400 1'] = {
+    'detail': 'This name already exists',
+    'status': 400,
+    'title': 'A server error occurred.',
+    'type': 'http://testserver/problems/error/'
+}
+
+snapshots['PutTests::test_put_invalid_data_returns_bad_request_1_invalid_id_json 1'] = {
+    'detail': 'Validation Error',
+    'errors': {
+        'id': [
+            'A valid integer is required.'
+        ]
+    },
+    'status': 400,
+    'title': 'Invalid input.',
+    'type': 'http://testserver/problems/error/'
+}
+
+snapshots['PutTests::test_put_invalid_data_returns_bad_request_2_invalid_name_json 1'] = {
+    'detail': 'Validation Error',
+    'errors': {
+        'name': [
+            'Enter a valid value.',
+            'Ensure this field has no more than 50 characters.'
+        ]
+    },
+    'status': 400,
+    'title': 'Invalid input.',
+    'type': 'http://testserver/problems/error/'
+}
+
+snapshots['PutTests::test_put_invalid_data_returns_bad_request_3_invalid_description_json 1'] = {
+    'detail': 'Validation Error',
+    'errors': {
+        'description': [
+            'Enter a valid value.',
+            'Ensure this field has no more than 200 characters.'
+        ]
+    },
+    'status': 400,
+    'title': 'Invalid input.',
+    'type': 'http://testserver/problems/error/'
+}
+
+snapshots['PutTests::test_put_invalid_data_returns_bad_request_4_invalid_missing_fields_json 1'] = {
+    'detail': 'Validation Error',
+    'errors': {
+        'name': [
+            'This field is required.'
+        ]
+    },
+    'status': 400,
+    'title': 'Invalid input.',
+    'type': 'http://testserver/problems/error/'
+}
+
+snapshots['PutTests::test_put_invalid_id_returns_bad_request 1'] = {
+    'detail': 'Invalid parameters',
+    'status': 400,
+    'title': 'Request has invalid parameters',
+    'type': 'http://testserver/problems/error/'
+}
+
+snapshots['PutTests::test_put_invalid_json_returns_bad_request 1'] = {
+    'detail': 'Parsing JSON Error',
+    'errors': 'JSON parse error - Expecting value: line 1 column 13 (char 12)',
+    'status': 400,
+    'title': 'Invalid input json.',
+    'type': 'http://testserver/problems/error/'
+}
+
+snapshots['PutTests::test_put_invalid_prod_data_returns_bad_request_1_basic_1_update_with_products_json 1'] = {
+    'detail': 'Validation Error',
+    'errors': {
+        'products': [
+            'Invalid pk "3" - object does not exist.'
+        ]
+    },
+    'status': 400,
+    'title': 'Invalid input.',
+    'type': 'http://testserver/problems/error/'
+}
+
+snapshots['PutTests::test_put_invalid_prod_data_returns_bad_request_2_invalid_product_id_json 1'] = {
+    'detail': 'Validation Error',
+    'errors': {
+        'products': [
+            'Incorrect type. Expected pk value, received str.'
+        ]
+    },
+    'status': 400,
+    'title': 'Invalid input.',
+    'type': 'http://testserver/problems/error/'
+}
+
+snapshots['PutTests::test_put_non_existent_id_returns_not_found 1'] = {
+    'detail': 'Not Found',
+    'status': 404,
+    'title': 'Resource not found',
+    'type': 'http://testserver/problems/error/'
+}
+
 snapshots['PutTests::test_put_valid_returns_ok_1_basic_1_update_json 1'] = [
     {
         'description': 'Tag identifying a product as halal.',
@@ -257,6 +381,18 @@ snapshots['PutTests::test_put_valid_returns_ok_3_boundary_description_json 1'] =
         'id': 1,
         'name': 'Halal',
         'products': [
+        ],
+        'retired': False
+    }
+]
+
+snapshots['PutTests::test_put_with_prods_valid_returns_ok_1_basic_1_update_with_products_json 1'] = [
+    {
+        'description': 'Tag identifying a product as halal.',
+        'id': 1,
+        'name': 'Halal',
+        'products': [
+            3
         ],
         'retired': False
     }
--- answerking_app/tests/test_integration/test_tags.py
+++ answerking_app/tests/test_integration/test_tags.py
@@ -2,6 +2,7 @@ from django.test import Client
 from assertpy import assert_that
 from ddt import ddt, data
 
+from answerking_app.models.models import Tag, Product
 from answerking_app.tests.test_integration.IntegrationTestBaseClass import (
     IntegrationTestBase,
 )
@@ -132,7 +133,126 @@ class PutTests(IntegrationTestBase):
         assert_that(response.status_code).is_equal_to(200)
         self.assertMatchSnapshot(get_response.json())
 
+    @data("basic-1-update-with-products.json")
+    def test_put_with_prods_valid_returns_ok(self, tag_update_data):
+        seeded_tag_data = self.seedFixture("tags", "basic-1.json")
+        seeded_prod_data = self.seedFixture("products", "basic-3.json")
+        tag: Tag = Tag.objects.get(
+            pk=seeded_tag_data["id"]  # type: ignore[GeneralTypeIssue]
+        )
+        for prod_data in seeded_prod_data:
+            prod: Product = Product.objects.get(pk=prod_data["id"])
+            tag.products.add(prod)
+        put_data = self.getFixture("tags", tag_update_data)
+        response = client.put(
+            f"/api/tags/{seeded_tag_data['id']}",  # type: ignore[GeneralTypeIssue]
+            put_data,
+            content_type="application/json",
+        )
+        get_response = client.get("/api/tags")
+        assert_that(response.status_code).is_equal_to(200)
+        self.assertMatchSnapshot(get_response.json())
+
+    @data(
+        "invalid-id.json",
+        "invalid-name.json",
+        "invalid-description.json",
+        "invalid-missing-fields.json",
+    )
+    def test_put_invalid_data_returns_bad_request(self, tag_data):
+        seeded_tag_data = self.seedFixture("tags", "basic-1.json")
+        put_data = self.getFixture("tags", tag_data)
+        response = client.put(
+            f"/api/tags/{seeded_tag_data['id']}",  # type: ignore[GeneralTypeIssue]
+            put_data,
+            content_type="application/json",
+        )
+        self.assertJSONErrorResponse(response.json())
+        assert_that(response.status_code).is_equal_to(400)
+
+    @data("basic-1-update-with-products.json", "invalid-product-id.json")
+    def test_put_invalid_prod_data_returns_bad_request(self, tag_update_data):
+        seeded_tag_data = self.seedFixture("tags", "basic-1.json")
+        seeded_prod_data = self.seedFixture("products", "basic-3.json")
+        Product.objects.get(pk=3).delete()
+        tag: Tag = Tag.objects.get(
+            pk=seeded_tag_data["id"]  # type: ignore[GeneralTypeIssue]
+        )
+        for prod_data in seeded_prod_data[:2]:
+            prod: Product = Product.objects.get(pk=prod_data["id"])
+            tag.products.add(prod)
+        put_data = self.getFixture("tags", tag_update_data)
+        response = client.put(
+            f"/api/tags/{seeded_tag_data['id']}",  # type: ignore[GeneralTypeIssue]
+            put_data,
+            content_type="application/json",
+        )
+        self.assertJSONErrorResponse(response.json())
+        assert_that(response.status_code).is_equal_to(400)
+
+    def test_put_invalid_json_returns_bad_request(self):
+        self.seedFixture("tags", "basic-1.json")
+        invalid_json_data: str = '{"invalid": }'
+        response = client.put(
+            "/api/tags/1",
+            invalid_json_data,
+            content_type="application/json",
+        )
+        self.assertJSONErrorResponse(response.json())
+        assert_that(response.status_code).is_equal_to(400)
+
+    def test_put_invalid_id_returns_bad_request(self):
+        response = client.put("/api/categories/invalid-id")
+        self.assertJSONErrorResponse(response.json())
+        assert_that(response.status_code).is_equal_to(400)
+
+    def test_put_non_existent_id_returns_not_found(self):
+        response = client.put("/api/categories/1")
+        self.assertJSONErrorResponse(response.json())
+        assert_that(response.status_code).is_equal_to(404)
+
+    def test_put_duplicated_name_returns_400(self):
+        self.seedFixture("tags", "basic-1.json")
+        seeded_data_2 = self.seedFixture(
+            "tags", "basic-1-different-name.json"
+        )
+        put_data = self.getFixture(
+            "tags", "basic-1-update-dup-name.json"
+        )
+        response = client.put(
+            f"/api/tags/{seeded_data_2['id']}",  # type: ignore[GeneralTypeIssue]
+            put_data,
+            content_type="application/json",
+        )
+        self.assertJSONErrorResponse(response.json())
+        assert_that(response.status_code).is_equal_to(400)
+
 
 class DeleteTests(IntegrationTestBase):
-    pass
+    def test_delete_returns_ok(self):
+        seeded_data = self.seedFixture("tags", "basic-1.json")
+        cat_url = f"/api/tags/{seeded_data['id']}"  # type: ignore[GeneralTypeIssue]
+        response = client.delete(cat_url)
+        get_response = client.get(cat_url)
+        assert_that(response.status_code).is_equal_to(204)
+        assert_that(str(get_response.json()), None)
+
+    def test_delete_invalid_id_returns_bad_request(self):
+        response = client.delete("/api/tags/invalid-id")
+        self.assertJSONErrorResponse(response.json())
+        assert_that(response.status_code).is_equal_to(400)
+
+    def test_delete_non_existent_id_returns_not_found(self):
+        response = client.delete("/api/tags/1")
+        self.assertJSONErrorResponse(response.json())
+        assert_that(response.status_code).is_equal_to(404)
+
+    def test_delete_already_retired_returns_gone(self):
+        seeded_data = self.seedFixture("tags", "basic-1.json")
+        tag_url = f"/api/tags/{seeded_data['id']}"  # type: ignore[GeneralTypeIssue]
+        response_1 = client.delete(tag_url)
+        response_2 = client.delete(tag_url)
+        assert_that(response_1.status_code).is_equal_to(204)
+        self.assertJSONErrorResponse(response_2.json())
+        assert_that(response_2.status_code).is_equal_to(410)
 
